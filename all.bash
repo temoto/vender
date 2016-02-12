2f2d22dd5f61b239886e23b7386c202b845c14c0
Sergey Shepelev <temotor@gmail.com>
2016-02-12 19:11:45 +0500

    initial travis

--- all.bash
+++ all.bash
@@ -1,6 +1,36 @@
 #!/bin/bash
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-( cd "$base/avr-spi-uart" ; make clean all )
-for d in display display/lcd-test head ; do
-  ( cd "$base/$d" ; go get ; go build )
-done
+app="$1"
+set -e
+
+build_go() {
+	if [[ -z "$app" ]] || [[ "$app" == "$1" ]]; then
+		cd "$base/$1"
+		go get .
+		go build
+		if ls ./*_test.go >/dev/null 2>&1; then
+			go test
+			go test -bench=.
+		fi
+	fi
+}
+
+main() {
+	if [[ -z "$app" ]] || [[ "$app" == "avr-spi-uart" ]]; then
+		cd "$base/avr-spi-uart"
+		echo -n "CC version: "
+		${CC-cc} --version
+		echo -n "avr-gcc version: "
+		avr-gcc --version
+		make clean all
+	fi
+
+	build_go crc
+	build_go display
+	build_go display/lcd-test
+	build_go head
+	build_go i2c
+	build_go i2c/i2c-test
+}
+
+main
