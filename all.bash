7c0b2d19391a947328519ef9bb7fe904675cb364
Sergey Shepelev <temotor@gmail.com>
2018-06-22 23:49:05 +0500

    travis: go only 1.10, build all, go test -race, codecov.io

--- all.bash
+++ all.bash
@@ -1,39 +1,29 @@
 #!/bin/bash
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-: ${app=""}
-cmd="$1"
 set -eu
-
-dodir() {
-    echo "- $1" >&2
-    (
-		cd "$base/$1"
-		if ls ./*.go >/dev/null 2>&1; then
-			do_go
-		else
-            echo "skip unknown target: $1" >&2
-        fi
-    )
-}
-
-do_go() {
-    go get
-    go build
-    if ls ./*_test.go >/dev/null 2>&1; then
-        go test
-        go test -bench=.
-    fi
-}
 
 main() {
 	cd $base
-    if [[ -n "$app" ]] ; then
-        dodir "$app"
-    else
-        for d in $(find . -type d ! -path '.' ! -path './.*' ! -path './archive*') ; do
-            dodir "$d"
-        done
-    fi
+
+    go get ./...
+    go build ./...
+
+    # begin workaround cannot use test profile flag with multiple packages
+    for d in $(go list ./... |egrep -v '(vendor|archive)/') ; do
+        go test -race -coverprofile=$base/coverage-$(basename $d).txt -covermode=atomic $d
+    done
+    cat $base/coverage-*.txt >$base/coverage.txt
+    rm $base/coverage-*.txt
+    # end workaround cannot use test profile flag with multiple packages
+
+    go test -bench=. ./...
+
+    for d in $(find . -type d ! -path '.' ! -path './.*' ! -path './archive*') ; do
+        if ! ls $base/$d/*.go >/dev/null 2>&1 ; then
+            echo "no build defined for $d" >&2
+            exit 1
+        fi
+    done
 }
 
 main
