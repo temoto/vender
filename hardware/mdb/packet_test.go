6709a625701526cbc8919c94d312818e8c86dff6
Sergey Shepelev <temotor@gmail.com>
2018-09-25 00:47:57 +0500

    mdb: test invalid packet constructor

--- hardware/mdb/packet_test.go
+++ hardware/mdb/packet_test.go
@@ -1,7 +1,9 @@
 package mdb
 
 import (
+	"bytes"
 	"encoding/hex"
+	"strings"
 	"testing"
 )
 
@@ -28,6 +30,27 @@ func TestPacketFromHexToWire(t *testing.T) {
 			wireRaw := hex.EncodeToString(p.Wire(false))
 			if wireRaw != c.expect {
 				t.Errorf("wire='%s' expected='%s'", wireRaw, c.expect)
+			}
+		})
+	}
+}
+
+func TestInvalidPacketFrom(t *testing.T) {
+	t.Parallel()
+	type Case struct {
+		name string
+		fun  func() *Packet
+	}
+	cases := []Case{
+		Case{"hex", func() *Packet { return PacketFromHex("invalid hex") }},
+		Case{"bytes", func() *Packet { return PacketFromBytes(bytes.Repeat([]byte{'.'}, 41)) }},
+		Case{"string", func() *Packet { return PacketFromString(strings.Repeat("long", 20)) }},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			p := c.fun()
+			if p != nil {
+				t.Fatalf("expected nil parsed='%x'", p.Wire(false))
 			}
 		})
 	}
