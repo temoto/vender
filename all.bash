073ee6c5521f61aaa908609e7f88fb67ab15f5cd
Sergey Shepelev <temotor@gmail.com>
2018-11-15 20:10:57 +0500

    pigpio test, rust coverage fail

--- all.bash
+++ all.bash
@@ -1,18 +1,25 @@
 #!/bin/bash
 base="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-${build_rust:=1}
-${build_go:=1}
+: ${build_rust:=1}
+: ${build_go:=1}
 set -eu
 
 main() {
 	cd $base
-	rm -f $base/coverage.txt
+	rm -f $base/coverage.{info,txt} $base/*.gcda $base/*.gcno
 
 	git submodule update --init
 
 	if [[ "$build_rust" != "0" ]] ; then
-		cargo build -vv --all --release
-		cargo test -vv --all --release
+		cargo build -vv --release
+		cargo test -vv --release
+		# cargo cov clean --all-crates
+		# cargo cov build -vv --release
+		# cargo cov test -vv --release
+		# cargo cov report
+		# lcov --capture --directory . --base-directory . -o cov1
+		# lcov --extract cov1 "$base/*" -o coverage.info
+		# rm -f cov1
 	fi
 
 	if [[ "$build_go" != "0" ]] ; then
@@ -32,7 +39,7 @@ main() {
 		go test -bench=. ./...
 	fi
 
-	paths=$(find . -type d ! -path '.' ! -path '*/.*' ! -path './script*' ! -path './vendor*')
+	paths=$(find . -type d ! -path '.' ! -path '*/.*' ! -path './script*' ! -path './target*' ! -path './vendor*')
 	for d in $paths ; do
 		# skip directories without files
 		if [[ -z "$(find $base/$d -depth 1 ! -type d ! -path '*/.*')" ]] ; then continue ; fi
