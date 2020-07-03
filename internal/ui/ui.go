f1f2a30bd9f97029270a6d7233d660b39d6f9e3a
AlexTransit <alexey.milko@gmail.com>
2020-07-03 18:22:28 +0500

    set default UI messages

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -50,6 +50,22 @@ func (self *UI) Init(ctx context.Context) error {
 		return err
 	}
 	self.g.Log.Debugf("menu len=%d", len(self.menu))
+	if self.g.Config.UI.Front.MsgMenuCodeEmpty == "" {self.g.Config.UI.Front.MsgMenuCodeEmpty = "Code empty"}
+	if self.g.Config.UI.Front.MsgMenuCodeInvalid == "" {self.g.Config.UI.Front.MsgMenuCodeInvalid = "Code invalid"}
+	if self.g.Config.UI.Front.MsgMenuInsufficientCredit == "" {self.g.Config.UI.Front.MsgMenuInsufficientCredit = "Insufficient credit"}
+	if self.g.Config.UI.Front.MsgMenuNotAvailable == "" {self.g.Config.UI.Front.MsgMenuNotAvailable = "Not available"}
+	if self.g.Config.UI.Front.MsgMenuError == "" {self.g.Config.UI.Front.MsgMenuError = "menu error"}
+	if self.g.Config.UI.Front.MsgCream == "" {self.g.Config.UI.Front.MsgCream = "Cream"}
+	if self.g.Config.UI.Front.MsgSugar == "" {self.g.Config.UI.Front.MsgSugar = "Sugar"}
+	if self.g.Config.UI.Front.MsgCredit == "" {self.g.Config.UI.Front.MsgCredit = "Credit"}
+	if self.g.Config.UI.Front.MsgMaking1 == "" {self.g.Config.UI.Front.MsgMaking1 = "Making text line1"}
+	if self.g.Config.UI.Front.MsgMaking2 == "" {self.g.Config.UI.Front.MsgMaking2 = "Making text line2"}
+	if self.g.Config.UI.Front.MsgInputCode == "" {self.g.Config.UI.Front.MsgInputCode = "Code :%s\x00"}
+	if self.g.Config.UI.Front.MsgStateBroken == "" {self.g.Config.UI.Front.MsgStateBroken = "mashine broken"}
+	if self.g.Config.UI.Front.MsgStateLocked == "" {self.g.Config.UI.Front.MsgStateLocked = "mashine locked"}
+	if self.g.Config.UI.Front.MsgStateIntro == "" {self.g.Config.UI.Front.MsgStateIntro = "reklama here"}
+	if self.g.Config.UI.Front.MsgWait == "" {self.g.Config.UI.Front.MsgWait = "please, wait"}
+	if self.g.Config.UI.Front.MsgWaterTemp == "" {self.g.Config.UI.Front.MsgWaterTemp = "temperature: %d"}
 
 	self.display = self.g.MustTextDisplay()
 	self.eventch = make(chan types.Event)
