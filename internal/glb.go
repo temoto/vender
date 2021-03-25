98a652999d1132785f645fc94089a46f8a3ca338
Alexey Milko <alexey.milko@gmail.com>
2021-03-25 18:50:03 +0500

    --

---
+++ internal/glb.go
@@ -0,0 +1,16 @@
+package global
+
+import (
+	"github.com/temoto/vender/log2"
+)
+
+var log = log2.NewStderr(log2.LInfo)
+
+func SetEnv(key string, val string) {
+	log.Info("AAAAAAAAAAAAAA")
+	os.Setenv(key, val)
+}
+
+func GetEnv(key string) string {
+	return os.Getenv(key)
+}
